17cf5d8d3ec9bac2f6d67bddce222026bdce9e35
sivaShilpa <shilpa.vchand@gmail.com>
2024-03-04 15:08:52 -0500

    box CSS done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,46 +1,15 @@
 import './Home.css'
 import React from 'react'
- 
+import '../../components/Box/Box.css' 
 const Home = () => {
   return (
-    <div>
-   
-      
+    <div className='goldBox'>
+        <div className='fullname'>Shilpa Sivarajan</div>
+        <div className='welcomeMessage'>Welcome to my Portfolio!</div>
     </div>
   )
 }
 
 export default Home
-// // import ReactDOM from 'react-dom';
-// var React = require('react')
 
-// function Home(props) {
-  
-
-//   return <div id="home-container">
-//     {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-//     {/* <div class="card"> */}
-//       <div class="name-home">
-//       {/* <div class="first-name"> */}
-//       <div class="color1">Shilpa</div>
-//       {/* </div> */}
-//       &nbsp;
-//       {/* <div class="last-name"> */}
-//       <div class="color2">Siva</div>
-//       <div class="color3">rajan</div>
-//       </div>
-//       {/* </div> */}
-//       <div>
-//       <h1 class="welcome">Welcome to my portfolio!</h1>
-//       </div>
-     
-//     <div className='navigation'>
-//       <Navigation />
-//     </div>
-   
-    
-//   </div>
-// }
-
-// export default Home;
 
